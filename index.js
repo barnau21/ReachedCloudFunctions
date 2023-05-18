a983a02d71d55c0c83476e87ca89c4c06a79ab9c
barnau21 <adumitriu21@gmail.com>
2023-05-18 13:11:21 +0300

    yet another fix in the firebase app initialization

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const name = "projects/reachedapp-64503/secrets/reached-cloud-credentials" +
 "/versions/1";
 
 /**
- * gets credentials from google secret
+ * gets credentials from google secred
  */
 async function getSecret() {
   const [version] = await client.accessSecretVersion({name});
@@ -19,9 +19,8 @@ async function getSecret() {
  * initializes app
  */
 async function initializeApp() {
-  const credentials = await getSecret();
   admin.initializeApp({
-    credential: admin.credential.cert(credentials),
+    credential: admin.credential.cert(await getSecret()),
     databaseURL: "https://reachedapp-64503-default-rtdb.firebaseio.com",
   });
   database = admin.database();
